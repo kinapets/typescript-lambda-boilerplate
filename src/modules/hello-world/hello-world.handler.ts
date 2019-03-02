77e946f4e27ce12cd086d99f5578a147dc2010d1
Radim Štěpaník <radim.stepanik@gmail.com>
2019-03-02 22:41:54 +0100

    refactor(hello-world): renaming

--- src/modules/hello-world/hello-world.handler.ts
+++
@@ -1,5 +0,0 @@
-import { APIGatewayEvent, APIGatewayProxyCallback, Context, Handler } from 'aws-lambda';
-
-export const helloWorldHandler: Handler = (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback) => {
-  callback(null, { body: JSON.stringify({ event, hello: 'world' }), statusCode: 200 });
-};
--- src/modules/hello-world/index.ts
+++ src/modules/hello-world/index.ts
@@ -1,1 +1,5 @@
-export { helloWorldHandler } from './hello-world.handler';
+import { APIGatewayEvent, APIGatewayProxyCallback, Context, Handler } from 'aws-lambda';
+
+export const helloWorldHandler: Handler = (event: APIGatewayEvent, context: Context, callback: APIGatewayProxyCallback) => {
+    callback(null, { body: JSON.stringify({ event, hello: 'world' }), statusCode: 200 });
+};
