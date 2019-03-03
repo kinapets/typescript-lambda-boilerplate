c6462b8aa0b2af1c2516c81717e06cde055f5d93
Radim Štěpaník <radim.stepanik@gmail.com>
2019-03-03 11:28:00 +0100

    feat(base): remove stuffs for qeetup

--- src/modules/s3-upload-image/index.ts
+++ src/modules/s3-upload-image/index.ts
@@ -1,5 +1,5 @@
 import { APIGatewayEvent, APIGatewayProxyCallback, Callback, Context, S3Event } from 'aws-lambda';
-import { Rekognition, S3, SNS, SQS } from 'aws-sdk';
+import { S3 } from 'aws-sdk';
 
 import * as _ from 'lodash';
 import * as uuid from 'uuid';
@@ -29,53 +29,5 @@ export const s3UploadImage = async (event: APIGatewayEvent, context: Context, ca
 };
 
 export const s3UploadImageSubsribe = async (event: S3Event, context: Context, callback: Callback) => {
-    const sns = new SNS();
-    const topicArn = `arn:aws:sns:${config.region}:${config.accountId}:${config.snsTopic}`;
-    try {
-        console.log(event.Records[0].s3.object.key); //tslint:disable-line
-        const params = {
-            SimilarityThreshold: 90,
-            SourceImage: {
-                S3Object: {
-                    Bucket: config.s3bucket,
-                    Name: 'me',
-                },
-            },
-            TargetImage: {
-                S3Object: {
-                    Bucket: config.s3bucket,
-                    Name: event.Records[0].s3.object.key,
-                },
-            },
-        };
-        const rekognition = new Rekognition();
-        const result = await rekognition.compareFaces(params).promise();
-        if (result.FaceMatches.length > 0) {
-            const sqs = new SQS();
-            await sqs
-                .sendMessage({ QueueUrl: config.sqsArn, MessageBody: JSON.stringify({ key: event.Records[0].s3.object.key }) })
-                .promise();
-        } else {
-            await sns
-                .publish({
-                    TopicArn: topicArn,
-                    Message: JSON.stringify({ key: event.Records[0].s3.object.key }),
-                })
-                .promise();
-        }
-        console.log(result); //tslint:disable-line
-
-        callback(null, event);
-    } catch (error) {
-        console.error(error, event); //tslint:disable-line
-        if (error.code === 'InvalidParameterException') {
-            await sns
-                .publish({
-                    TopicArn: topicArn,
-                    Message: JSON.stringify({ key: event.Records[0].s3.object.key }),
-                })
-                .promise();
-        }
-        callback(error, null);
-    }
+    callback(null, event);
 };
